f704c504c29812044dcd46c329b20d5d7e1f49fa
Desant71 <desant31081993@gmail.com>
2024-01-12 03:01:15 +0300

    Изменение в ветку longer

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        SalesManager sales = new SalesManager(new int[]{100, 200, 500, -2917, 50968});
+        SalesManager sales = new SalesManager(new long[]{100, 200, 500, -2917, 50968});
         System.out.println(sales.max());
     }
 }
